eb01a7c948700a526394377c06d8bf0967d9c1ef
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2025-06-02 23:16:29 -0400

    Initial commit

---
+++ src/root.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react"
+import reactLogo from "@/assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./root.css"
+import "./index.css"
+import { isRouteErrorResponse, Scripts, ScrollRestoration } from "react-router"
+import type { Route } from "./+types/root"
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div id="root">
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    )
+}
+
+export function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html>
+            <head>
+                <meta charSet="UTF-8" />
+                <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Vite + React + TS</title>
+            </head>
+            <body>
+                {children}
+                <ScrollRestoration />
+                <Scripts />
+            </body>
+        </html>
+    )
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+    let message = "Oops!"
+    let details = "An unexpected error occurred."
+    let stack: string | undefined
+
+    if (isRouteErrorResponse(error)) {
+        message = error.status === 404 ? "404" : "Error"
+        details =
+            error.status === 404
+                ? "The requested page could not be found."
+                : error.statusText || details
+    } else if (import.meta.env.DEV && error && error instanceof Error) {
+        details = error.message
+        stack = error.stack
+    }
+
+    return (
+        <main id="error-page">
+            <h1>{message}</h1>
+            <p>{details}</p>
+            {stack && (
+                <pre>
+                    <code>{stack}</code>
+                </pre>
+            )}
+        </main>
+    )
+}
+
+export default App
