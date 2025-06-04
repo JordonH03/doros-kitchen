e11abf49d1329e7114c3e95ec3141cb891fa7c66
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2025-06-03 23:55:43 -0400

    feat: Refactor project to ssr

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,7 +2,7 @@ import { type Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
-  ssr: false,
+  ssr: true,
   appDirectory: "src",
   presets: [vercelPreset()],
 } satisfies Config;
