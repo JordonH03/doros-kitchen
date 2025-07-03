6bf10243e925bcdc27fb11950e8e469df9b91e5f
JordonH03 <57729746+JordonH03@users.noreply.github.com>
2025-07-02 22:02:32 -0400

    refactor: Create SettingsField component

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -2,6 +2,7 @@ import { MAX_INTERVAL, MAX_TIME, MIN_INTERVAL, MIN_TIME } from "@/util/constants
 import { useTimer } from "@/util/timer/TimerContext"
 import type { ModalProps } from "@/util/types"
 import { useEffect, useRef } from "react"
+import SettingsField from "./settingsField"
 
 export default function Settings({ isOpen, onClose }: ModalProps) {
     const { state, dispatch } = useTimer()
@@ -17,7 +18,6 @@ export default function Settings({ isOpen, onClose }: ModalProps) {
         } else {
             modalElement.close()
         }
-
     }, [isOpen])
 
     useEffect(() => {
@@ -26,88 +26,82 @@ export default function Settings({ isOpen, onClose }: ModalProps) {
         const handleClose = () => {
             onClose()
         }
-        modalElement.addEventListener('close', handleClose)
+        modalElement.addEventListener("close", handleClose)
         return () => {
-            modalElement.removeEventListener('close', handleClose)
+            modalElement.removeEventListener("close", handleClose)
         }
     }, [onClose])
 
     const handleSave = () => {
-        const form = formRef.current as HTMLFormElement | null;
-        if (!form) return;
-        
-        if (!form.checkValidity()) {
-            form.reportValidity();
-            return;
-        } 
+        const form = formRef.current as HTMLFormElement | null
+        if (!form) return
 
-        dispatch({"type": "RESET_TIMER"})
-        const formData = new FormData(form);
-        
+        if (!form.checkValidity()) {
+            form.reportValidity()
+            return
+        }
+
+        dispatch({ type: "RESET_TIMER" })
+        const formData = new FormData(form)
+
         dispatch({
             type: "UPDATE_TIMER",
             payload: {
-                pomodoro: Number(formData.get('pomodoro')), 
-                shortBreak: Number(formData.get('shortBreak')), 
-                longBreak: Number(formData.get('longBreak')), 
-                interval: Number(formData.get('interval')) 
-            }
-        });
+                pomodoro: Number(formData.get("pomodoro")),
+                shortBreak: Number(formData.get("shortBreak")),
+                longBreak: Number(formData.get("longBreak")),
+                interval: Number(formData.get("interval")),
+            },
+        })
 
         onClose()
     }
 
     return (
         <dialog id="settings-modal" className="modal" ref={modalRef}>
-            <h2>Settings</h2>
+            <h2 className="form-heading">Settings</h2>
             <form className="settings-form" ref={formRef}>
-                <label>
-                    Pomodoro
-                    <input
-                        name="pomodoro"
-                        required
-                        type="number"
-                        max={MAX_TIME}
-                        min={MIN_TIME}
-                        defaultValue={state.pomodoro}
-                    />
-                </label>
-                <label>
-                    Short Break
-                    <input
-                        name="shortBreak"
-                        required
-                        type="number"
-                        max={MAX_TIME}
-                        min={MIN_TIME}
-                        defaultValue={state.shortBreak}
-                    />
-                </label>
-                <label>
-                    Long Break
-                    <input
-                        name="longBreak"
-                        required
-                        type="number"
-                        max={MAX_TIME}
-                        min={MIN_TIME}
-                        defaultValue={state.longBreak}
-                    />
-                </label>
-                <label>
-                    Long Break Interval
-                    <input
-                        required
-                        name="interval"
-                        type="number"
-                        max={MAX_INTERVAL}
-                        min={MIN_INTERVAL}
-                        defaultValue={state.interval}
-                    />
-                </label>
+                <SettingsField
+                    labelText="Pomodoro"
+                    fieldName="pomodoro"
+                    max={MAX_TIME}
+                    min={MIN_TIME}
+                    defaultValue={state.pomodoro}
+                />
+                <SettingsField
+                    labelText="Short Break"
+                    fieldName="shortBreak"
+                    max={MAX_TIME}
+                    min={MIN_TIME}
+                    defaultValue={state.shortBreak}
+                />
+                <SettingsField
+                    labelText="Long Break"
+                    fieldName="longBreak"
+                    max={MAX_TIME}
+                    min={MIN_TIME}
+                    defaultValue={state.longBreak}
+                />
+                <SettingsField
+                    labelText="Long Break Interval"
+                    fieldName="interval"
+                    max={MAX_INTERVAL}
+                    min={MIN_INTERVAL}
+                    defaultValue={state.interval}
+                />
                 <div className="cta__group">
-                    <input className="button cta__button" type="button" value="Cancel" onClick={onClose} />
-                    <input className="button cta__button" type="button" value="Save" onClick={handleSave} />
+                    <input
+                        className="button cta__button button__white"
+                        type="button"
+                        value="Cancel"
+                        onClick={onClose}
+                    />
+                    <input
+                        className="button cta__button button__primary"
+                        type="button"
+                        value="Save"
+                        onClick={handleSave}
+                    />
                 </div>
             </form>
         </dialog>
---
+++ app/components/settingsField.tsx
@@ -0,0 +1,31 @@
+import { MAX_TIME, MIN_TIME } from "@/util/constants"
+
+interface ISettingsField {
+    labelText: string
+    fieldName: string
+    max?: number
+    min?: number
+    defaultValue?: number
+}
+
+export default function SettingsField({
+    labelText,
+    fieldName,
+    max = MAX_TIME,
+    min = MIN_TIME,
+    defaultValue = 0,
+}: ISettingsField) {
+    return (
+        <div className="settings-field">
+            <label htmlFor={fieldName}>{labelText}</label>
+            <input
+                name={fieldName}
+                required
+                max={max}
+                min={min}
+                defaultValue={defaultValue}
+                type="number"
+            />
+        </div>
+    )
+}
